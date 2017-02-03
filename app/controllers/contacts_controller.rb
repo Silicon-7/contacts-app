554028d611db9b806cea9c1d0d179d54dec999ec
JackMarx <seriousfools@gmail.com>
2017-02-02 18:59:05 -0600

    update

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,12 @@
 class ContactsController < ApplicationController
-  def index
-      @contacts = Contact.all
+    def index
+      if current_user
+        @contacts = current_user.contacts
+        render "index.html.erb"
+      else
+        flash[:warning] = 'You must be logged in to see this page'
+        redirect_to '/login'
+      end
     end
 
     def show
@@ -17,7 +23,8 @@ class ContactsController < ApplicationController
                              last_name: params[:last_name], 
                              email: params[:email], 
                              phone_number: params[:phone_number],
-                             bio: params[:bio]
+                             bio: params[:bio],
+                             user_id: current_user.id
                              )
       @contact.save
       flash[:success] = "Contact created."
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+User.create(name: "Charlie", email: "chocolate@factory.com", password: "password", password_confirmation: "password")
+User.create(name: "Willy", email: "willy@factory.com", password: "password", password_confirmation: "password")
+
+
 100.times do
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
@@ -8,6 +12,7 @@
                  last_name: last_name,
                  email: Faker::Internet.free_email("#{first_name}.#{last_name}"),
                  phone_number: Faker::PhoneNumber.phone_number,
-                 bio: Faker::Hipster.paragraph(10, false, 8)
+                 bio: Faker::Hipster.paragraph(10, false, 8),
+                 user_id: rand(1..2)
                  )
 end
